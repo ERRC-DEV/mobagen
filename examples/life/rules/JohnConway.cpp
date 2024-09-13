a1188eb83bdfd60ccc311e424a136bb9ab258e28
StoneSteve24 <ethan.chevalier@mymail.champlain.edu>
2024-09-13 11:10:13 -0400

    Neighbor check complete

--- examples/life/rules/JohnConway.cpp
+++ examples/life/rules/JohnConway.cpp
@@ -7,5 +7,24 @@ void JohnConway::Step(World& world) {
 
 int JohnConway::CountNeighbors(World& world, Point2D point) {
   // todo: implement
-  return 0;
+  bool neighbors[8];
+  int neighborNumber = 0;
+  int numNeighbors = 0;
+  for (int i = -1; i < 2; i++)
+  {
+    for (int j = -1; j < 2; j++)
+    {
+      neighbors[neighborNumber] = world.Get(Point2D(point.x+i, point.y+j));
+      neighborNumber++;
+    }
+  }
+  for (int i = 0; i < 8; i++)
+  {
+    if (neighbors[i])
+    {
+      numNeighbors++;
+    }
+  }
+
+  return numNeighbors;
 }
