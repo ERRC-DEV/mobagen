f04b78baaf14963a3fc6442a20e4800dc0c7b508
StoneSteve24 <ethan.chevalier@mymail.champlain.edu>
2024-10-11 15:21:01 -0400

    Effectively complete. Checking about heuristic and will update if it needs a new behavior

--- examples/catchthecat/Agent.cpp
+++ examples/catchthecat/Agent.cpp
@@ -22,18 +22,18 @@ int Agent::getHeuristic(Point2D point, int sideSize) {
   }
   if(point.x + point.y > 0) {
     if(point.x - point.y > 0) {
-      return (sideSize / 2) - point.x;
+      return abs((sideSize / 2) - point.x);
     }
-    else if(point.x - point.y < 0) {
-      return (sideSize / 2) - point.y;
+    else {
+      return abs((sideSize / 2) - point.y);
     }
   }
-  else if(point.x + point.y < 0) {
+  else {
     if (point.x - point.y > 0) {
-      return (-sideSize / 2) + point.y;
+      return abs((-sideSize / 2) + point.y);
     }
-    else if(point.x - point.y < 0) {
-      return (-sideSize / 2) + point.x;
+    else {
+      return abs((-sideSize / 2) + point.x);
     }
   }
 }
@@ -43,6 +43,7 @@ std::vector<Point2D> Agent::generatePath(World* w) {
   priority_queue<ASNode> queue;                   // to store next ones to visit
   unordered_map<Point2D, bool> visited;      // use .at() to get data, if the element dont exist [] will give you wrong results
 
+  std::vector<Point2D> path;
   // bootstrap state
   auto catPos = w->getCat();
   queue.push({catPos, 0, w->getWorldSideSize()/2});
@@ -53,50 +54,61 @@ std::vector<Point2D> Agent::generatePath(World* w) {
     auto current = queue.top();
     queue.pop();
     visited[current.point] = true;
-    std::vector<Point2D> neighbors = getVisitableNeighbors(w, catPos);
-    // iterate over the neighs:
+    if (w->catWinsOnSpace(current.point)) {
+      borderExit = current.point;
+      break;
+    }
+    std::vector<Point2D> neighbors = getVisitableNeighbors(w, current.point);
     for (auto neighbor : neighbors) {
       if (!visited[neighbor]) {
         // Push to the queue
         cameFrom.emplace(neighbor, current.point);
-        // just push them all, it is automatically sorted
+        queue.push({neighbor, current.accDist+1, getHeuristic(neighbor, w->getWorldSideSize())});
       }
     }
     // for every neighbor set the cameFrom
     // enqueue the neighbors to frontier and frontierset
     // do this up to find a visitable border and break the loop
   }
-
   // if the border is not infinity, build the path from border to the cat using the camefrom map
   // if there isnt a reachable border, just return empty vector
   // if your vector is filled from the border to the cat, the first element is the catcher move, and the last element is the cat move
-  return vector<Point2D>();
+  if (borderExit != Point2D::INFINITE) {
+    Point2D pathPoint = borderExit;
+    while (pathPoint != catPos) {
+      path.push_back(pathPoint);
+      pathPoint = cameFrom[pathPoint];
+    }
+
+  }
+
+  return path;
 }
 
 std::vector<Point2D> Agent::getVisitableNeighbors(World* w, Point2D p) {
   vector<Point2D> neighbors;
   Point2D nextPoint = w->NE(p);
-  if (w->isValidPosition(nextPoint) && w->catCanMoveToPosition(nextPoint)) {
+  if (w->isValidPosition(nextPoint) && !w->getContent(nextPoint)) {
     neighbors.push_back(nextPoint);
   }
   nextPoint = w->NW(p);
-  if (w->isValidPosition(nextPoint) && w->catCanMoveToPosition(nextPoint)) {
+  if (w->isValidPosition(nextPoint) && !w->getContent(nextPoint)) {
     neighbors.push_back(nextPoint);
   }
   nextPoint = w->E(p);
-  if (w->isValidPosition(nextPoint) && w->catCanMoveToPosition(nextPoint)) {
+  if (w->isValidPosition(nextPoint) && !w->getContent(nextPoint)) {
     neighbors.push_back(nextPoint);
   }
   nextPoint = w->W(p);
-  if (w->isValidPosition(nextPoint) && w->catCanMoveToPosition(nextPoint)) {
+  if (w->isValidPosition(nextPoint) && !w->getContent(nextPoint)) {
     neighbors.push_back(nextPoint);
   }
   nextPoint = w->SW(p);
-  if (w->isValidPosition(nextPoint) && w->catCanMoveToPosition(nextPoint)) {
+  if (w->isValidPosition(nextPoint) && !w->getContent(nextPoint)) {
     neighbors.push_back(nextPoint);
   }
   nextPoint = w->SE(p);
-  if (w->isValidPosition(nextPoint) && w->catCanMoveToPosition(nextPoint)) {
+  if (w->isValidPosition(nextPoint) && !w->getContent(nextPoint)) {
     neighbors.push_back(nextPoint);
   }
 
--- examples/catchthecat/Catcher.cpp
+++ examples/catchthecat/Catcher.cpp
@@ -6,6 +6,13 @@ Point2D Catcher::Move(World* world) {
   for (;;) {
     Point2D p = {Random::Range(-side, side), Random::Range(-side, side)};
     auto cat = world->getCat();
-    if (cat.x != p.x && cat.y != p.y && !world->getContent(p)) return p;
+    std::vector<Point2D> path = generatePath(world);
+    if (!path.empty()) {
+      return path.front();
+    }
+    else {
+      if (cat.x != p.x && cat.y != p.y && !world->getContent(p)) return p;
+    }
+
   }
 }
