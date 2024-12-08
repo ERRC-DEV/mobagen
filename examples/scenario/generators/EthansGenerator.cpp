73bc82ab90e79c399c05f5603b6676daaad743c2
ethan.chevalier <ethan.chevalier@mymail.champlain.edu>
2024-12-07 19:32:51 -0500

    Finished

--- examples/scenario/generators/EthansGenerator.cpp
+++ examples/scenario/generators/EthansGenerator.cpp
@@ -19,6 +19,7 @@ std::vector<Color32> EthansGenerator::Generate(int sideSize, float time) {
     // get time difference
     // run erode with the dt
     float const dt = time - timeOfLastUpdate;
+    doErosion(dt);
     timeOfLastUpdate = time;
   }
 
@@ -49,13 +50,7 @@ void EthansGenerator::generateElevationMap(float displacement)
       elevation /= TOTAL_AMPLITUDE;
       elevation = pow(elevation, REDISTRIBUTION_EXPONENT);
 
-      // Testing
-      //elevationMap[y][x] = test;
-      // Force islands
-      // GOOD LORD FIX THE DISTANCE CALCULATION THIS LOOKS SO DAMN WRONG
       float distance = 1.0f - ((1.0f-(nx*nx)) * (1.0f-(ny*ny)));
-      //float distance = 1.0f;
-      //distance -= pow((((float)x-(float)sideSizeCached)*((float)x-(float)sideSizeCached)) * (((float)y-(float)sideSizeCached)*((float)y-(float)sideSizeCached)),0.5f);
 
       elevation = std::lerp(elevation, 1-distance, LERP_MIX);
 
@@ -86,10 +81,81 @@ std::vector<Color32> EthansGenerator::heightsToColor()
   return colors;
 }
 
-void EthansGenerator::doErosion(float dt)
+void EthansGenerator::doErosion(float dt)  // Code base/pseudocode is taken from particle generator as inspiration and modified to suit my generator's needs
 {
+  //glm::vec3 getSurfaceNormal();
 
+  static int cycles = 1;
+  if (dt > 0.500) {
+    if (cycles > 1) cycles--;
+  } else
+    cycles++;
+
+  for (int i = 0; i < cycles; i++)
+  {
+    glm::vec2 startPos = glm::vec2(Random::Range(1, (int)sideSizeCached - 2), Random::Range(1, (int)sideSizeCached - 2));
+    WaterDroplet drop(startPos);
+
+    while (drop.volume > minVol)
+    {
+      // If it is out of bounds, destroy it
+      if ((int)drop.pos.x >= sideSizeCached || (int)drop.pos.y >= sideSizeCached || (int)drop.pos.x <= 0 || (int)drop.pos.y <= 0)
+      {
+        break;
+      }
+      /*if (elevationMap[(int)drop.pos.x][(int)drop.pos.y] < 0.5f) Toyed around with having it break when it hits the water level. Did not like it
+      {
+        break;
+      }*/
+
+      glm::vec3 normal = getSurfaceNormal((int)drop.pos.x, (int)drop.pos.y); // Normal of surface
+      glm::vec2 initialPos = glm::vec2(drop.pos.x, drop.pos.y); // The inital position
+
+      //Move the drop
+      drop.speed += dt * glm::vec2(normal.x, normal.z) / (drop.volume * density);
+      drop.pos += dt * drop.speed;
+      drop.speed *= (1.0 - dt * friction);
+
+      // If this moved it out of bounds, kill it
+      if (drop.pos.x >= sideSizeCached || drop.pos.y >= sideSizeCached || drop.pos.x <= 0 || drop.pos.y <= 0)
+      {
+        break;
+      }
+
+      float maxsediment = drop.volume * glm::length(drop.speed) * (elevationMap[(int)initialPos.x][(int)initialPos.y] - elevationMap[(int)drop.pos.x][(int)drop.pos.y]);
+      if (maxsediment < 0.0) maxsediment = 0.0;
+      float sdiff = maxsediment - drop.sediment;
+
+      drop.sediment += dt * depositionRate * sdiff;
+      elevationMap[(int)initialPos.x][(int)initialPos.y] -= dt * drop.volume * depositionRate * sdiff;
+
+      drop.volume *= (1.0f - dt * evapRate);
+    }
+  }
 }
+
+glm::vec3 EthansGenerator::getSurfaceNormal(int x, int y)
+{
+  glm::vec3 normal = glm::vec3(0, 0, 0);
+  float scale = 5;
+
+  static const float sqrt2 = sqrtf(2);
+
+  // Cardinal directions
+  normal += glm::vec3(0.15) * glm::normalize(glm::vec3(scale * (elevationMap[x][y] - elevationMap[x + 1][y]), 1.0, 0.0)); //East
+  normal += glm::vec3(0.15) * glm::normalize(glm::vec3(scale * (elevationMap[x - 1][y] - elevationMap[x][y]), 1.0, 0.0)); //West
+  normal += glm::vec3(0.15) * glm::normalize(glm::vec3(0.0, 1.0, scale * (elevationMap[x][y] - elevationMap[x][y + 1]))); //North
+  normal += glm::vec3(0.15) * glm::normalize(glm::vec3(0.0, 1.0, scale * (elevationMap[x][y - 1] - elevationMap[x][y]))); //South
+
+  // Diagonal
+  normal += glm::vec3(0.1) * glm::normalize(glm::vec3(scale * (elevationMap[x][y] - elevationMap[x + 1][y + 1]) / sqrt2, sqrt2,scale * (elevationMap[x][y] - elevationMap[x + 1][y + 1]) / sqrt2));  // Positive Y
+  normal += glm::vec3(0.1) * glm::normalize(glm::vec3(scale * (elevationMap[x][y] - elevationMap[x + 1][y - 1]) / sqrt2, sqrt2,scale * (elevationMap[x][y] - elevationMap[x + 1][y - 1]) / sqrt2));  // Positive Y
+  normal += glm::vec3(0.1) * glm::normalize(glm::vec3(scale * (elevationMap[x][y] - elevationMap[x - 1][y + 1]) / sqrt2, sqrt2,scale * (elevationMap[x][y] - elevationMap[x - 1][y + 1]) / sqrt2));  // Positive Y
+  normal += glm::vec3(0.1) * glm::normalize(glm::vec3(scale * (elevationMap[x][y] - elevationMap[x - 1][y - 1]) / sqrt2, sqrt2,scale * (elevationMap[x][y] - elevationMap[x - 1][y - 1]) / sqrt2));  // Positive Y
+
+  return normal;
+}
+
 
 
 std::string EthansGenerator::GetName() { return "EthanGenerator"; }
--- examples/scenario/generators/EthansGenerator.h
+++ examples/scenario/generators/EthansGenerator.h
@@ -20,13 +20,29 @@ private:
   std::vector<Color32> heightsToColor();
   void generateElevationMap(float displacement);
   void doErosion(float dt);
+  glm::vec3 getSurfaceNormal(int x, int y);
 
   // Erosion constants
-
+  float minVol = 0.01;
+  float density = 1;
+  float friction = 0.05;
+  float depositionRate = 0.1;
+  float evapRate = 0.001;
 public:
   std::vector<Color32> Generate(int sideSize, float displacement = 0) override;
   std::string GetName() override;
 
+};
+
+struct WaterDroplet {
+  // Construct Particle at Position
+  WaterDroplet(glm::vec2 _pos) { pos = _pos; }
+
+  glm::vec2 pos;
+  glm::vec2 speed = glm::vec2(0.0);
+
+  float volume = 1.0;    // This will vary in time
+  float sediment = 0.0;  // Fraction of Volume that is Sediment!
 };
 
 #endif //ETHANSGENERATOR_H
